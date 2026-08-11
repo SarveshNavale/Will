641a2d7c3b48dd084bbc9f11af4c87251cddc798
SarveshNavale <sarveshnavale18@gmail.com>
2026-08-11 17:56:33 +0530

    VarName changes

--- Assets/Scripts/EnvironmentGenerator.cs
+++ Assets/Scripts/EnvironmentGenerator.cs
@@ -6,33 +6,34 @@ public class EnvironmentGenerator : MonoBehaviour
 {
     [SerializeField] private SpriteShapeController _spriteShapeController;
 
-    [SerializeField, Range(3f, 100f)] private int _levelLength = 50;
-    [SerializeField, Range(1f, 50f)] private float _xMultiplier = 2f;
-    [SerializeField, Range(1f, 50f)] private float _yMultiplier = 2f;
-    [SerializeField, Range(0f, 1f)] private float _curveSmoothness = 0.5f;
+    [SerializeField, Range(3f, 100f)] private int _platformWidth = 50;
+    [SerializeField] private float _platformHeight = 10f;
+    [SerializeField, Range(1f, 50f)] private float _xDistance = 2f;
+    [SerializeField, Range(1f, 50f)] private float _yDistance = 2f;
+
     [SerializeField] private float _noiseStep = 0.5f;
-    [SerializeField] private float _bottom = 10f;
+    [SerializeField, Range(0f, 1f)] private float _tangentSmoothness = 0.5f;
 
     private Vector3 _lastPos;
 
     private void OnValidate()
     {
-       _spriteShapeController.spline.Clear();
+        _spriteShapeController.spline.Clear();
 
-       for (int i =0; i < _levelLength; i++)
+        for (int i =0; i < _platformWidth; i++)
         {
-            _lastPos = new Vector3(i * _xMultiplier, Mathf.PerlinNoise(0, i* _noiseStep)*_yMultiplier);
+            _lastPos = new Vector3(i * _xDistance, Mathf.PerlinNoise(0, i* _noiseStep)*_yDistance);
             _spriteShapeController.spline.InsertPointAt(i,  _lastPos);
 
-            if(i!=0 && i != _levelLength - 1)
+            if(i!=0 && i != _platformWidth - 1)
             {
                 _spriteShapeController.spline.SetTangentMode(i, ShapeTangentMode.Continuous);
-                _spriteShapeController.spline.SetLeftTangent(i, Vector3.left * _xMultiplier * _curveSmoothness);
-                _spriteShapeController.spline.SetRightTangent(i, Vector3.right * _xMultiplier * _curveSmoothness);
+                _spriteShapeController.spline.SetLeftTangent(i, Vector3.left * _xDistance * _tangentSmoothness);
+                _spriteShapeController.spline.SetRightTangent(i, Vector3.right * _xDistance * _tangentSmoothness);
             }
         }
 
-        _spriteShapeController.spline.InsertPointAt(_levelLength, new Vector3(_lastPos.x, -_bottom));
-        _spriteShapeController.spline.InsertPointAt(_levelLength+1, new Vector3(0, -_bottom));
+        _spriteShapeController.spline.InsertPointAt(_platformWidth, new Vector3(_lastPos.x, -_platformHeight));
+        _spriteShapeController.spline.InsertPointAt(_platformWidth+1, new Vector3(0, -_platformHeight));
     }
 }
